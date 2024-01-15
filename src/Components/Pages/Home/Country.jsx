e0194d2c3778d80b02edd79ffee8f40a2c3208c6
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-01-15 16:00:00 +0600

    add toggle button

--- src/Components/Pages/Home/Country.jsx
+++ src/Components/Pages/Home/Country.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 
 const Country = ({ country }) => {
 
-    console.log(country);
 
     const { id, thumbnailUrl, title, url } = country || {};
+
+    const [visited, setVisited] = useState(false);
+    const handleVisited = () => {
+        setVisited(!visited);
+        console.log(visited);
+    }
 
 
     return (
@@ -20,6 +26,10 @@ const Country = ({ country }) => {
                     <h1><span className="font-bold">Capital :</span> {country.capital[0]}</h1>
                     <h1><span className="font-bold">Area :</span> {country.area}</h1>
                     <h1><span className="font-bold">Population :</span> {country.population}</h1>
+                </div>
+
+                <div>
+                    <button onClick={handleVisited} className={visited ? "btn bg-green-500" : "btn bg-red-400"} >{visited ? "visited" : "Not Visited"}</button>
                 </div>
 
             </div>
