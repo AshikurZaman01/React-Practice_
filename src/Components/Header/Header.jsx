0ec5315551a42a7c7a8d171bdb9bb5831a73e3f6
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-02-04 15:39:11 +0600

    navbar with a tag

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,55 +1,27 @@
-import { useState } from "react";
-import { MdOutlineMenuOpen } from "react-icons/md";
-import { FaWindowClose } from "react-icons/fa";
 
 const Header = () => {
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    const routes = [
+    const items = [
         { id: 1, path: '/', name: 'Home' },
-        { id: 2, path: '/contact', name: 'Contact' },
-        { id: 3, path: '/help', name: 'Help' },
-        { id: 4, path: '/about', name: 'About' },
-        { id: 5, path: '/login', name: 'Login' }, // Add this line to include the login route in the header 
+        { id: 2, path: '/about', name: 'About' },
+        { id: 3, path: '/contact', name: 'Contact' },
+        { id: 4, path: '/help', name: 'Help' },
     ]
-
-
 
     return (
         <div>
-            <div>Navbar</div>
-            <nav>
-                <h1>Navbar</h1>
-
-                <div onClick={() => setIsOpen(!isOpen)} className="md:hidden text-[50px]">
+            <div>
+                <ul>
                     {
-                        isOpen ? <FaWindowClose /> : <MdOutlineMenuOpen />
-                    }
-
-                </div>
-
-                <ul className={isOpen ? "" : 'md:flex hidden bg-blue-300 h-[80px] items-center gap-10 text-3xl font-bold mb-10 px-5 duration-1000 transition-all'}>
-                    {
-                        routes.map(route => {
+                        items.map(item => {
                             return (
-                                <li key={route.id}><a href={route.path}>{route.name}</a></li>
+                                <li key={item.id}><a href={item.path}>{item.name}</a></li>
                             )
                         })
                     }
                 </ul>
-            </nav>
-
-            {/* <nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                    <li><a href="/help">Help</a></li>
-                    <li><a href="/about">About</a></li>
-                </ul>
-            </nav> */}
-
-        </div >
+            </div>
+        </div>
     );
 };
 
