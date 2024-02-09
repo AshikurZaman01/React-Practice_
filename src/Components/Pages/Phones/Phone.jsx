13343e5d23c7706362818e1b6298e6a6d9449ddb
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-02-09 23:15:04 +0600

    call data with AXIOS

--- src/Components/Pages/Phones/Phone.jsx
+++ src/Components/Pages/Phones/Phone.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
 
@@ -5,10 +6,15 @@ const Phone = () => {
 
     const [phones, setPhones] = useState([]);
 
+    // useEffect(() => {
+    //     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    //         .then(res => res.json())
+    //         .then(data => setPhones(data.data))
+    // }, [])
+
     useEffect(() => {
-        fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-            .then(res => res.json())
-            .then(data => setPhones(data.data))
+        axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+            .then(data => setPhones(data.data.data))
     }, [])
 
     console.log(phones);
