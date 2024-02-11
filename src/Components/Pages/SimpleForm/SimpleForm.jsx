016254f3743c170ea9257b7f2af303f0b57357b2
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-02-11 16:09:30 +0600

    recap usehooks

--- src/Components/Pages/SimpleForm/SimpleForm.jsx
+++ src/Components/Pages/SimpleForm/SimpleForm.jsx
@@ -1,47 +1,37 @@
 import { useState } from "react";
+import useInputState from "./useInput";
 
 const SimpleForm = () => {
 
-    const [name, setName] = useState(null);
-    const [email, setEmail] = useState(null);
-    const [password, setPassword] = useState(null);
-    const [error, setError] = useState(null);
+    const [name, handleName] = useInputState('');
+    const [email, handleEmail] = useInputState('');
+    const [password, handlePassword] = useInputState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
         console.log(name, email, password);
     }
 
-    const handleName = (e) => {
-        setName(e.target.value);
-    }
-
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-    }
-
-    const handlePassword = (e) => {
-        setPassword(e.target.value);
-    }
 
     return (
-        <div className='my-10'>
+        <div className="my-10">
             <h1>Handle Form</h1>
 
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="">Name</label>
-                <input onChange={handleName} type="text" name="name" id="" placeholder='Enter Name' />
-                <br /><br />
-                <label htmlFor="">Email</label>
-                <input onChange={handleEmail} type="text" name="email" id="" placeholder='Enter Email' />
-                <br /><br />
-                <label htmlFor="">Password</label>
-                <input onChange={handlePassword} type="text" name="password" placeholder='Password' id="" />
-                <br /><br />
-                <button type='submit'>Submit</button>
-            </form>
+            <div className="border border-red-400 p-10">
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="">Name</label><br />
+                    <input value={name} onChange={handleName} type="text" name="name" id="" placeholder="Enter Your Name" /><br /><br />
 
+                    <label htmlFor="">Email</label><br />
+                    <input value={email} onChange={handleEmail} type="text" name="email" placeholder="Enter Your Email" id="" />
+                    <br /><br />
+
+                    <label htmlFor="">Password</label><br />
+                    <input value={password} onChange={handlePassword} type="text" name="password" id="" placeholder="Enter Password" /><br /><br />
+
+                    <button type="submit" className="btn btn-success btn-sm">Submit</button>
+                </form>
+            </div>
         </div>
     );
 };
