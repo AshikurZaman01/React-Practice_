bef1c3dae5dda1d9eb69af53c16fd17df661df64
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-03-19 20:55:01 +0600

    useRef use in form

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,32 +1,52 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { LuEye } from "react-icons/lu";
+import { LuEyeOff } from "react-icons/lu";
 
 const Home = () => {
 
+    const nameRef = useRef(null);
+    const ageRef = useRef(null);
+    const passwordRef = useRef(null);
+
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
 
-        const name = e.target.name.value;
-        const age = e.target.age.value;
-
-        console.log(name, age);
+        console.log(nameRef.current.value);
+        console.log(ageRef.current.value);
+        console.log(passwordRef.current.value);
     }
 
-    const [open, isOpen] = useState(false)
-
+    const [view, setView] = useState(false);
 
     return (
-        <div>
+        <div className="flex justify-center items-center h-[80vh] ">
 
-            <form onSubmit={handleSubmit} className="bg-blue-300 w-[400px] flex flex-col mx-auto mt-10 rounded-md p-4">
-                <label className="" htmlFor="">Enter Name</label>
-                <input type="text" name="name" className="rounded-md" id="" /><br />
+            <form onClick={handleSubmit} className="w-[300px] bg-blue-300 p-5">
 
-                <input type="number" name="age" id="" /><br />
+                <div className="flex flex-col">
+                    <label htmlFor="" className="text-slate-900">Name</label>
+                    <input ref={nameRef} type="text" name="" id="" placeholder="Enter Name" className="rounded-sm p-1" />
+                </div><br />
 
-                <input type={`${open ? "text" : "password"}`} name="password" id="" />
-                <button onClick={() => isOpen(!open)} className="btn btn-sm btn-error w-[100px]">Show Password</button><br />
+                <div className="flex flex-col">
+                    <label htmlFor="" className="text-slate-900">Age</label>
+                    <input ref={ageRef} type="number" name="" id="" placeholder="Enter Age" className="rounded-sm p-1" />
+                </div><br />
 
-                <button type="submit" className="btn btn-sm btn-accent">Submit</button>
+                <div className="flex flex-col relative">
+                    <label htmlFor="" className="text-slate-900">Password</label>
+                    <input ref={passwordRef} type={`${view ? "text" : "password"}`} name="" id="" placeholder="Enter Password" className="rounded-sm p-1 " />
+                    <span onClick={() => setView(!view)} className="absolute right-2 top-10 transform -translate-y-1/2 cursor-pointer text-xl text-gray-500  w-6 z-40 flex justify-center">
+                        {
+                            view ? <LuEye /> : <LuEyeOff />
+                        }
+                    </span>
+                </div><br />
+
+                <div>
+                    <button className="btn btn-sm w-full btn-info">Submit</button>
+                </div>
+
             </form>
 
         </div>
