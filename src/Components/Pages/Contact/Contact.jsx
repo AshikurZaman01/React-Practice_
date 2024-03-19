f6b06db49c2b493de4799eee1ecd4a306f7671dd
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-03-19 21:23:12 +0600

    updated

--- src/Components/Pages/Contact/Contact.jsx
+++ src/Components/Pages/Contact/Contact.jsx
@@ -1,8 +1,23 @@
+import { useRef } from "react";
+import Form from "../Home/Form";
 
 const Contact = () => {
+
+    const nameRef = useRef(null);
+    const ageRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    const handleSignSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(nameRef.current.value);
+        console.log(ageRef.current.value);
+        console.log(passwordRef.current.value);
+    }
+
     return (
         <div>
-            This is contact page
+            T<Form nameRef={nameRef} ageRef={ageRef} passwordRef={passwordRef} title={"Sign Up"} btnSubmit="Sign Up" handleSubmit={handleSignSubmit}></Form>
         </div>
     );
 };
