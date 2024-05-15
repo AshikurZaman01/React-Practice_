457a9fe465be95c951f4866ca6f9a5d137918613
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-15 12:55:11 +0600

    add form

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,73 +1,23 @@
-import { data } from "autoprefixer";
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React from 'react';
 
 const Home = () => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const id = e.target.id.value;
-        const productsName = e.target.products.value;
-
-        if (isNaN(id) && !isNaN(productsName)) {
-            alert('please enter a valid number')
-        }
-        else {
-            const dataArray = JSON.parse(localStorage.getItem('data') || '[]');
-
-            const newData = {
-                id: id,
-                product: productsName
-            }
-
-            dataArray.push(newData);
-            localStorage.setItem('data', JSON.stringify(dataArray));
-        }
-    }
-
-    const [showData, setShowData] = useState([]);
-    useEffect(() => {
-
-        const dataArray = JSON.parse(localStorage.getItem('data') || '[]');
-        setShowData(dataArray);
-    }, [])
-
-
-    const handleDelete = (id) => {
-
-        const newDataArray = showData.filter((data, indx) => indx !== id);
-        localStorage.setItem('data', JSON.stringify(newDataArray));
-        setShowData(newDataArray);
-
-
-    }
-
-
+    
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <input type="text" name="id" placeholder="Enter Price" />
-                <input type="text" name="products" placeholder="Enter Number of Products" />
-                <button type="submit">Submit</button>
-            </form>
 
-            <div className="mt-10">
-                {
-                    showData.map((data, indx) => {
-                        return (
-                            <div key={indx} >
-                                <ul className="flex gap-5">
-                                    <li>Price :{data.id}</li>
-                                    <li>P.Name : {data.product}</li>
-                                    <button onClick={() => handleDelete(indx)} className="btn btn-sm btn-primary">Edit</button>
-                                </ul>
-                            </div>
-                        )
-                    })
-                }
+            <div>
+                <h1 className='text-center text-4xl font-bold my-4'>Input Details</h1>
 
+                <div className='flex justify-center items-center my-5'>
+                    <form className='w-[400px]'>
+                        <input type="text" placeholder='Enter Your Name' className="input input-bordered w-full max-w-xs" />
+                        <br />
+                        <input type="text" placeholder='Enter Your Email' className="input input-bordered w-full max-w-xs" />
+                        <br />
+                        <button type='submit' className='btn btn-primary w-full max-w-xs mt-2'>Submit</button>
+                    </form>
+                </div>
             </div>
 
         </div>
