432a19d8a45b2a200e3d94cc18f823b13df325f3
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-15 11:44:37 +0600

    add push data

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -4,23 +4,31 @@ const Home = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(e.target.id.value);
 
-        const idNum = e.target.id.value;
+        const id = e.target.id.value;
+        const productsName = e.target.products.value;
 
-        if (isNaN(idNum)) {
-            alert("Please enter a number")
+        if (isNaN(id) && !isNaN(productsName)) {
+            alert('please enter a valid number')
         }
         else {
-            localStorage.setItem("id", idNum);
-        }
+            const dataArray = JSON.parse(localStorage.getItem('data') || '[]');
 
+            const newData = {
+                id: id,
+                product: productsName
+            }
+
+            dataArray.push(newData);
+            localStorage.setItem('data', JSON.stringify(dataArray));
+        }
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="id" />
+                <input type="text" name="id" placeholder="Enter Price" />
+                <input type="text" name="products" placeholder="Enter Number of Products" />
                 <button type="submit">Submit</button>
             </form>
         </div>
