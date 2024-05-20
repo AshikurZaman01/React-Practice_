812d6ea16dd8dd7c8c206a1c5512ab99620b8984
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-20 23:19:36 +0600

    add btn and themecard

---
+++ src/Components/Pages/Home/Context/Theme.js
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+
+
+export const ThemeContext = createContext(
+    {
+        themeMode: "light",
+        darkTheme: () => { },
+        lightTheme: () => { },
+    }
+);
+
+
+export const ThemeProvider = ThemeContext.Provider;
+
+
+export const useTheme = () => useContext(ThemeContext);
--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,15 +1,49 @@
-import { UserContextProvider } from "./Context/UserContextProvider";
-import Login from "./Login";
-import Profile from "./Profile";
+import React, { useEffect, useState } from 'react';
+import { ThemeProvider } from './Context/Theme';
+import ThemeBtn from './ThemeBtn';
+import Card from './Card';
 
 const Home = () => {
+
+    const [themeMode, setThemeMode] = useState('light');
+
+    const lightTheme = () => {
+        setThemeMode('light');
+
+    }
+    const darkTheme = () => {
+        setThemeMode('dark');
+    }
+
+
+    useEffect(() => {
+
+        document.querySelector('html').classList.remove('light', 'dark');
+        document.querySelector('html').classList.add(themeMode);
+
+    }, [themeMode])
+
     return (
-        <UserContextProvider>
+
+        <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+
             <div>
-                <Login></Login>
-                <Profile></Profile>
+
+                <div className='flex flex-wrap min-h-screen items-center'>
+                    <div className="w-full">
+                        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                            <ThemeBtn></ThemeBtn>
+                        </div>
+
+                        <div className="w-full max-w-sm mx-auto">
+                            <Card></Card>
+
+                        </div>
+
+                    </div>
+                </div>
             </div>
-        </UserContextProvider>
+        </ThemeProvider>
     );
 };
 
